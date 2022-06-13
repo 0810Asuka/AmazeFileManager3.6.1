f1279b8176a698bea0db925d916aecc7b1b16e06
0810Asuka <100206850+0810Asuka@users.noreply.github.com>
2022-06-13 17:38:48 +0800

    Add files via upload

---
+++ bug#2647/script-#2647.py
@@ -0,0 +1,48 @@
+import uiautomator2 as u2
+import sys
+import time
+d(resourceId="com.amaze.filemanager:id/sd_main_fab").click()
+d.xpath('//*[@resource-id="com.amaze.filemanager:id/menu_new_file"]/android.widget.ImageButton[1]').click()
+d.swipe(0.133, 0.262, 0.213, 0.264)
+d(resourceId="android:id/floating_toolbar_menu_item_text", text="Cut").click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_shift"]/android.widget.ImageView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_1_4"]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_2_1"]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_bottom_symbol_2"]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_shift"]/android.widget.ImageView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_0_4"]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_1_0"]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_0_3"]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_bottom_symbol_2"]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_1_4"]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_2_1"]/android.widget.TextView[1]').click()
+d(resourceId="com.amaze.filemanager:id/md_buttonDefaultPositive").click()
+d(resourceId="com.amaze.filemanager:id/search").click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_1_4"]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_2_1"]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_bottom_symbol_2"]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_0_4"]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_1_0"]/android.widget.LinearLayout[1]/android.widget.FrameLayout[1]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_0_3"]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_bottom_symbol_2"]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_1_4"]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_2_1"]/android.widget.TextView[1]').click()
+d.xpath('//*[@resource-id="com.google.android.inputmethod.latin:id/key_pos_ime_action"]/android.widget.FrameLayout[1]/android.widget.ImageView[2]').click()
+d.xpath('//*[@resource-id="com.android.systemui:id/ends_group"]/android.widget.RelativeLayout[2]').click()
+d(resourceId="com.amaze.filemanager:id/properties").click()
+d.xpath('//android.widget.ListView/android.widget.LinearLayout[9]/android.widget.LinearLayout[1]/android.widget.RelativeLayout[1]').click()
+
+while True:
+    d.service("uiautomator").stop()
+    time.sleep(2)
+    out = d.service("uiautomator").running()
+    if not out:
+        print("DISCONNECT UIAUTOMATOR2 SUCCESS")
+        break
+    time.sleep(2)
+    
+
+
+
+    
+    
